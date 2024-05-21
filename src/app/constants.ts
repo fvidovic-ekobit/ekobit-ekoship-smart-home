6a72b88bb37a37273386a61da8ffe462e586f8e9
Filip Vidović <filip.vidovic@rods-cones.com>
2024-05-21 10:24:47 +0200

    switched to prod api url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,5 @@
-// export const apiURL = 'https://ekoshipsmarthome.azurewebsites.net/';
-export const apiURL = 'https://localhost:7169/';
+// production
+export const apiURL = 'https://ekoshipsmarthome.azurewebsites.net/';
+
+// development
+// export const apiURL = 'https://localhost:7169/';
